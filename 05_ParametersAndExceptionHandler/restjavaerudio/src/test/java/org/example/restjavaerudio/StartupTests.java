a5b14d16dd780130b909795e40335725060188f9
Matheus Pacheco <70149366+killtheweb@users.noreply.github.com>
2024-05-27 18:24:17 -0300

    Exceptions

---
+++ 05_ParametersAndExceptionHandler/restjavaerudio/src/test/java/org/example/restjavaerudio/StartupTests.java
@@ -0,0 +1,16 @@
+package org.example.restjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class StartupTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
